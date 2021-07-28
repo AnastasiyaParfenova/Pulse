7acf811451acb6c91aae74ac31d2b6711cfddaa9
Anastasiya <parfenova.anastasiya.98@yandex.ru>
2021-07-28 16:12:25 +0500

    first commit

---
+++ src/mailer/smart.php
@@ -0,0 +1,47 @@
+<?php 
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'ssl://smtp.yandex.ru';
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'ignatowa.anastasya2017';                 // Наш логин
+$mail->Password = 'ghfuyvrvhljfoxtu';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+ 
+$mail->setFrom('ignatowa.anastasya2017', 'Pulse');   // От кого письмо 
+$mail->addAddress('parfenova.anastasiya.98@yandex.ru');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Данные';
+$mail->Body    = '
+		Пользователь оставил данные <br> 
+	Имя: ' . $name . ' <br>
+	Номер телефона: ' . $phone . '<br>
+	E-mail: ' . $email . '';
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
